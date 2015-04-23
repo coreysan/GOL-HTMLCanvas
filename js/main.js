c7bb1de273a501b9b2bacb9df96786de31903b4f
Corey Sanford(win) <csanford@actcommunity.net>
2015-04-23 11:37:25 -0700

    Bare-bones project assembled

---
+++ js/main.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-of-life', 
+                            { preload: preload, create: create });
+
+  function preload () {
+
+     game.load.image('logo', 'phaser.png');
+  }
+
+  function create () {
+
+    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+    logo.anchor.setTo(0.5, 0.5);
+  }
+};
