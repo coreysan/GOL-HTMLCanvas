0ae8b7d4a59ce68e4b80c4a34eb8dd8be06416c2
Corey Sanford(win) <csanford@actcommunity.net>
2015-04-28 15:52:53 -0700

    refactored using function object

--- js/main.js
+++ js/main.js
@@ -1,290 +1,351 @@
 (function () {
   'use strict';
-	var tiles, nexttiles, isPlaying, canvasW, canvasH, squaresPerBoard, ctx;
 
-	var stepDelay, stepInterval, onFillColor;
-  canvasW = canvasH = 512;
-	squaresPerBoard = 32;
-	isPlaying = false;
-	onFillColor = "#f99";
-	stepDelay = 30;
 
-	/* Initializes the tiles with the given width/height
-	*/
-	function populateBoard(tilesW, tilesH)
-	{
-		var y, x;
-		tiles = new Array(tilesH);
-		nexttiles = new Array(tilesH);
-	  for (y = 0; y < tilesH; y+=1)
-	  {
-	  	tiles[y] = new Array(tilesW);
-	  	nexttiles[y] = new Array(tilesW);
-	  	for (x = 0; x < tilesW; x+=1)
-	 	 	{
-	  		tiles[y][x] = 0;
-	  		nexttiles[y][x] = 0;
-	  	}
-	  }
-	}
+	function Board(){
+		this.squaresPerBoard = 32;
+		this.canvasW = this.canvasH = 512;
+		this.isPlaying = false;
+		this.onFillColor = "#f99";
+		this.stepDelay = 128;
+		this.stepInterval = null;
 
-	function drawGrid() {
-		var i;
-		ctx.strokeStyle="#eee";
+		this.tiles = new Array(this.squaresPerBoard);
+		this.nexttiles = new Array(this.squaresPerBoard);
+		this.ctx = document.getElementById("game-of-life").getContext("2d");
 
-	  for (i = 0.5; i < canvasW; i+= (canvasW/squaresPerBoard))
-	  {
-			ctx.moveTo(i,0);//draw vertical line
-			ctx.lineTo(i,canvasH);
-			ctx.stroke();
 
-			ctx.moveTo(0,i);//horizontal line
-			ctx.lineTo(canvasW,i);
-			ctx.stroke();
-	  }
-	}
-
-	function drawTile(x, y, color) {
-
-		// debugger;
-		ctx.beginPath();
-		ctx.fillStyle=color;
-		ctx.rect(x*(canvasW/squaresPerBoard), y*(canvasW/squaresPerBoard),
-								(canvasW/squaresPerBoard), (canvasH/squaresPerBoard));
-		ctx.fill();
-	}
-
-	function clearTiles(){
-		var y, x;
-
-		for (y = 0; y < squaresPerBoard; y+=1)
-	  {
-	  	for (x = 0; x < squaresPerBoard; x+=1)
-	 	 	{
-	  		tiles[y][x] = 0;
-	  		nexttiles[y][x] = 0;
-	  	}
-	  }
-	}
-	function clearNexttiles(){
-		var y, x;
-
-		for (y = 0; y < squaresPerBoard; y+=1)
-	  {
-	  	for (x = 0; x < squaresPerBoard; x+=1)
-	 	 	{
-	  		nexttiles[y][x] = 0;
-	  	}
-	  }
-	}
-	function swapTiles(){
-		var y, x;
-
-		for (y = 0; y < squaresPerBoard; y+=1)
-	  {
-	  	for (x = 0; x < squaresPerBoard; x+=1)
-	 	 	{
-	  		tiles[y][x] = nexttiles[y][x];
-	  		nexttiles[y][x] = 0;
-	  	}
-	  }
-	}
-
-	function clearBoard () {
-
-		ctx.clearRect(0, 0, canvasW, canvasH);
-	}
-
-	function determineNewBoard(){
-		var y, x, neighbours;
-		for (y = 0; y < squaresPerBoard; y+=1)
-	  {
-	  	for (x = 0; x < squaresPerBoard; x+=1)
-	 	 	{
-
-	 	 		neighbours = 0;
-	 	 		//left side
-	  		if(y-1 >= 0 && x-1 >= 0)
-	  		{
-	  			neighbours += tiles[y-1][x-1];
-	  		}
-	  		if(x-1 >= 0)
-	  		{
-	  			neighbours += tiles[y][x-1];
-	  		}
-	  		if(y+1 < squaresPerBoard-1 && x-1 >= 0)
-	  		{
-	  			neighbours += tiles[y+1][x-1];
-	  		}
-	  		//right side
-	  		if(y-1 >= 0 && x+1 < squaresPerBoard-1)
-	  		{
-	  			neighbours += tiles[y-1][x+1];
-	  		}
-	  		if(x+1 < squaresPerBoard-1)
-	  		{
-	  			neighbours += tiles[y][x+1];
-	  		}
-	  		if(y+1 < squaresPerBoard && x+1 < squaresPerBoard-1)
-	  		{
-	  			neighbours += tiles[y+1][x+1];
-	  		}
-	  		//top middle
-	  		if(y-1 >= 0)
-	  		{
-	  			neighbours += tiles[y-1][x];
-	  		}
-	  		//bottom middle
-	  		if(y+1 < squaresPerBoard-1)
-	  		{
-	  			neighbours += tiles[y+1][x];
-	  		}
-
-	  		if(neighbours < 2 || neighbours > 3)
-	  		{
-	  			nexttiles[y][x] = 0;
-	  			// console.log('cell '+x+','+y+' dies');
-	  		}else{
-	  			if(neighbours == 3) 
-	  			{
-	  				nexttiles[y][x] = 1;
-	  				// console.log('cell '+x+','+y+' lives');
-	  			}else if(tiles[y][x] == 1 && neighbours == 2) 
-	  			{
-	  				nexttiles[y][x] = 1;
-	  				// console.log('cell '+x+','+y+' lives');
-	  			}
-	  		}
-
-	  	}//for X
-	  }//for Y
-
-	  if(tiles == nexttiles)
-	  {
-	  	//we got stuck...
-	  	console.log("got stuck");
-	  	stopPlayback();
-	  }
-	  swapTiles();
-	}//determineNewBoard
-
-	function drawBoard() {
-		var x, y;
-
-		clearBoard();
-		drawGrid();
-		determineNewBoard();
-
-	  for (y = 0; y < squaresPerBoard; y+=1)
-	  {
-	  	for (x = 0; x < squaresPerBoard; x+=1)
-	 	 	{
-	  		if(tiles[y][x] == 1)
-	  		{
-	  			drawTile(x, y, onFillColor);
-	  		}
-	  	}
-	  }
-	}
-
-	function clickedCanvas (event) {
-
-		isPlaying = false;//stop playback when clicked
-
-		//translate x/y to square coordinates
-		var realX = Math.floor((event.layerX)/(canvasW/squaresPerBoard));
-		var realY = Math.floor((event.layerY)/(canvasW/squaresPerBoard));
-		// console.log("Square "+realX+", "+realY);
-
-		tiles[realY][realX] = (tiles[realY][realX] + 1)%2;//switch between 0 & 1
-		// debugger;
-
-		ctx.beginPath();
-		if(tiles[realY][realX] == 1)
-		{
-			ctx.fillStyle=onFillColor;
-		}else{
-			ctx.fillStyle="#FFF";
-		}
-		ctx.rect(realX*(canvasW/squaresPerBoard), realY*(canvasW/squaresPerBoard), (canvasW/squaresPerBoard), (canvasH/squaresPerBoard));
-		ctx.fill();
-	}
-
-	function redrawBoard(){
-
-		if(isPlaying == false)
-		{
-			clearInterval(stepInterval);
-			return false;
+		this.init = function(){
+			this.initTiles(this.squaresPerBoard);
+			this.drawGridLines();
 		}
 
-		drawBoard();
-	}
-
-	function stopPlayback(){
-			isPlaying = false;
-			document.getElementById("playpause").innerHTML = "Play";
-			clearInterval(stepInterval);
-	}
-
-	function clickedPlayPause(){
-
-		//redraw tiles over and over. 
-		if(isPlaying)
-		{
-			stopPlayback();
-		}else
-		{
-			isPlaying = true;
-			document.getElementById("playpause").innerHTML = "Pause";
-			//start timer
-			stepInterval = setInterval(redrawBoard, stepDelay);
+		this.initTiles = function(tilesW){
+			var y, x;
+			this.tiles = new Array(tilesW);
+			this.nexttiles = new Array(tilesW);
+		  for (y = 0; y < tilesW; y+=1)
+		  {
+		  	this.tiles[y] = new Array(tilesW);
+		  	this.nexttiles[y] = new Array(tilesW);
+		  	for (x = 0; x < tilesW; x+=1)
+		 	 	{
+		  		this.tiles[y][x] = 0;
+		  		this.nexttiles[y][x] = 0;
+		  	}
+		  }
 		}
-	}
+
+		this.drawGridLines = function() {
+			var i;
+			this.ctx.strokeStyle="#fff";
+
+		  for (i = 0.5; i < this.canvasW; i+= (this.canvasW/this.squaresPerBoard))
+		  {
+				this.ctx.moveTo(i,0);//draw vertical line
+				this.ctx.lineTo(i,this.canvasH);
+				this.ctx.stroke();
+
+				this.ctx.moveTo(0,i);//horizontal line
+				this.ctx.lineTo(this.canvasW,i);
+				this.ctx.stroke();
+		  }
+		}
+
+		this.drawTile = function (x, y, color) {
+
+			this.ctx.beginPath();
+			this.ctx.fillStyle=color;
+			this.ctx.rect(x*(this.canvasW/this.squaresPerBoard), y*(this.canvasW/this.squaresPerBoard),
+									(this.canvasW/this.squaresPerBoard), (this.canvasH/this.squaresPerBoard));
+			this.ctx.fill();
+		}
+
+		this.clearTiles = function (){
+
+			var y, x;
+			for (y = 0; y < this.squaresPerBoard; y+=1)
+		  {
+		  	for (x = 0; x < this.squaresPerBoard; x+=1)
+		 	 	{
+		  		this.tiles[y][x] = 0;
+		  		this.nexttiles[y][x] = 0;
+		  	}
+		  }
+		}
+
+		this.clearNexttiles = function (){
+			var y, x;
+
+			for (y = 0; y < this.squaresPerBoard; y+=1)
+		  {
+		  	for (x = 0; x < this.squaresPerBoard; x+=1)
+		 	 	{
+		  		this.nexttiles[y][x] = 0;
+		  	}
+		  }
+		}
+
+		this.swapTiles = function (){
+
+			var y, x;
+			for (y = 0; y < this.squaresPerBoard; y+=1)
+		  {
+		  	for (x = 0; x < this.squaresPerBoard; x+=1)
+		 	 	{
+		  		this.tiles[y][x] = this.nexttiles[y][x];
+		  		this.nexttiles[y][x] = 0;
+		  	}
+		  }
+		}
+
+		this.clearBoard = function () {
+
+			this.ctx.clearRect(0, 0, this.canvasW, this.canvasH);
+		}
+
+		this.determineNewBoard = function (){
+
+			var y, x, neighbours;
+			for (y = 0; y < this.squaresPerBoard; y+=1)
+		  {
+		  	for (x = 0; x < this.squaresPerBoard; x+=1)
+		 	 	{
+
+		 	 		neighbours = 0;
+		 	 		//left side
+		  		if(y-1 >= 0 && x-1 >= 0)
+		  		{
+		  			neighbours += this.tiles[y-1][x-1];
+		  		}
+		  		if(x-1 >= 0)
+		  		{
+		  			neighbours += this.tiles[y][x-1];
+		  		}
+		  		if(y+1 < this.squaresPerBoard-1 && x-1 >= 0)
+		  		{
+		  			neighbours += this.tiles[y+1][x-1];
+		  		}
+		  		//right side
+		  		if(y-1 >= 0 && x+1 < this.squaresPerBoard-1)
+		  		{
+		  			neighbours += this.tiles[y-1][x+1];
+		  		}
+		  		if(x+1 < this.squaresPerBoard-1)
+		  		{
+		  			neighbours += this.tiles[y][x+1];
+		  		}
+		  		if(y+1 < this.squaresPerBoard && x+1 < this.squaresPerBoard-1)
+		  		{
+		  			neighbours += this.tiles[y+1][x+1];
+		  		}
+		  		//top middle
+		  		if(y-1 >= 0)
+		  		{
+		  			neighbours += this.tiles[y-1][x];
+		  		}
+		  		//bottom middle
+		  		if(y+1 < this.squaresPerBoard-1)
+		  		{
+		  			neighbours += this.tiles[y+1][x];
+		  		}
+
+		  		if(neighbours < 2 || neighbours > 3)
+		  		{
+		  			this.nexttiles[y][x] = 0;
+		  			// console.log('cell '+x+','+y+' dies');
+		  		}else{
+		  			if(neighbours == 3) 
+		  			{
+		  				this.nexttiles[y][x] = 1;
+		  				// console.log('cell '+x+','+y+' lives');
+		  			}else if(this.tiles[y][x] == 1 && neighbours == 2) 
+		  			{
+		  				this.nexttiles[y][x] = 1;
+		  				// console.log('cell '+x+','+y+' lives');
+		  			}
+		  		}
+
+		  	}//for X
+		  }//for Y
+
+		  if(this.tiles == this.nexttiles)
+		  {
+		  	//we got stuck...
+		  	console.log("got stuck");
+		  	this.stopPlayback();
+		  }
+		  this.swapTiles();
+		}//determineNewBoard()
+
+		this.drawBoard = function () {
+			var x, y;
+
+			this.clearBoard();
+			this.drawGridLines();
+
+		  for (y = 0; y < this.squaresPerBoard; y+=1)
+		  {
+		  	for (x = 0; x < this.squaresPerBoard; x+=1)
+		 	 	{
+		  		if(this.tiles[y][x] == 1)
+		  		{
+		  			this.drawTile(x, y, this.onFillColor);
+		  		}
+		  	}
+		  }
+		}//drawBoard()
+
+		this.clickedCanvas = function(event) {
+
+			this.isPlaying = false;//stop playback when clicked
+
+			//translate x/y to square coordinates
+			var realX = Math.floor((event.layerX)/(this.canvasW/this.squaresPerBoard));
+			var realY = Math.floor((event.layerY)/(this.canvasW/this.squaresPerBoard));
+			// console.log("Square "+realX+", "+realY);
+
+			this.tiles[realY][realX] = (this.tiles[realY][realX] + 1)%2;//switch between 0 & 1
+
+			this.ctx.beginPath();
+			if(this.tiles[realY][realX] == 1)
+			{
+				this.ctx.fillStyle=this.onFillColor;
+			}else{
+				this.ctx.fillStyle="#FFF";//erase the marker
+			}
+			this.ctx.rect(realX*(this.canvasW/this.squaresPerBoard), realY*(this.canvasW/this.squaresPerBoard), (this.canvasW/this.squaresPerBoard), (this.canvasH/this.squaresPerBoard));
+			this.ctx.fill();
+		}
+
+		this.mouseOverCanvas = function(event) {
+
+			var realX = Math.floor((event.layerX)/(this.canvasW/this.squaresPerBoard));
+			var realY = Math.floor((event.layerY)/(this.canvasW/this.squaresPerBoard));
+
+			this.drawBoard();//redraw the board to clear any old hover-markers
+
+			//todo: 
+
+			//draw faint gridlines surrounding the mouse-hover, like:
+			// __|_|__
+			// __|_|__
+			//   | |
 
 
-	function clickedClear(){
-		
-		stopPlayback();
-		clearTiles();
-		clearBoard();
-		drawGrid();
-	}
-	function clickedFaster(){
-		
-		console.log("clickedFaster");
-	}
-	function clickedSlower(){
-		
-		console.log("clickedSlower");
-	}
-	function clickedZoomOut(){
-		
-		console.log("clickedZoomOut");
-	}
-	function clickedZoomIn(){
-		
-		console.log("clickedZoomIn");
-	}
+			//draw grey in the center
+			this.ctx.beginPath();
+			this.ctx.fillStyle="#ddd";
+			this.ctx.rect(realX*(this.canvasW/this.squaresPerBoard), realY*(this.canvasW/this.squaresPerBoard), (this.canvasW/this.squaresPerBoard), (this.canvasH/this.squaresPerBoard));
+			this.ctx.fill();
+
+			console.log("Mouse Over"+realX+", "+ realY);
+
+		}
+
+		this.redrawBoard = function(){
+
+			if(this.isPlaying == false)
+			{
+				clearInterval(this.stepInterval);
+				return false;
+			}
+			// debugger;
+			this.drawBoard();
+		}
+
+		this.stopPlayback = function() {
+				this.isPlaying = false;
+				document.getElementById("playpause").innerHTML = "Play";
+				clearInterval(this.stepInterval);
+		}
+
+		this.clickedPlayPause = function() {
+
+			// debugger;
+			//redraw tiles over and over. 
+			if(this.isPlaying)
+			{
+				this.stopPlayback();
+			}else
+			{
+				this.isPlaying = true;
+				document.getElementById("playpause").innerHTML = "Pause";
+				//start timer
+				this.restartTimer();
+			}
+		}
+
+		this.restartTimer = function(){
+
+			clearInterval(this.stepInterval);
+			this.stepInterval = setInterval( 
+					(function(self) {				//Self-Executing-Anonymous-Function
+         		return function() {   //Return a function in the context of 'self'
+
+							self.determineNewBoard();
+            	self.redrawBoard(); //Thing you wanted to run as non-window 'this'
+         		}
+     			})(this), this.stepDelay);
+
+		}
+
+		this.clickedClear = function(){
+			
+			this.stopPlayback();
+			this.clearTiles();
+			this.clearBoard();
+			this.drawGridLines();
+		}
+		this.clickedFaster = function(){
+			
+			if(this.stepDelay >= 32)
+			{
+				this.stepDelay /= 2;
+				this.restartTimer();
+			}
+			console.log("clickedFaster; now at "+this.stepDelay);
+			
+		}
+		this.clickedSlower = function(){
+			
+			if(this.stepDelay <= 2048)
+			{
+				this.stepDelay *= 2;
+				this.restartTimer();
+			}
+			console.log("clickedSlower; now at "+this.stepDelay);
+		}
+		this.clickedZoomOut = function(){
+			
+			console.log("clickedZoomOut");
+		}
+		this.clickedZoomIn = function(){
+			
+			console.log("clickedZoomIn");
+		}
 
 
+		this.init();
+	}//Board
 
+
+	
+	// board.changeName();
+	// debugger;
 
 	function main () {
-		var c; 
-		c = document.getElementById("game-of-life");
-		ctx = c.getContext("2d");
 
-		populateBoard(squaresPerBoard, squaresPerBoard);
-		drawGrid(ctx);
+		var board = new Board();
 
-		document.getElementById("game-of-life").addEventListener("click", clickedCanvas, false);
-		document.getElementById("playpause").addEventListener("click", clickedPlayPause, false);
-		document.getElementById("clear").addEventListener("click", clickedClear, false);
-		document.getElementById("faster").addEventListener("click", clickedFaster, false);
-		document.getElementById("slower").addEventListener("click", clickedSlower, false);
-		document.getElementById("zoom-out").addEventListener("click", clickedZoomOut, false);
-		document.getElementById("zoom-in").addEventListener("click", clickedZoomIn, false);
+		document.getElementById("game-of-life").addEventListener("click", function(event){board.clickedCanvas(event);}, false);
+		document.getElementById("game-of-life").addEventListener("mousemove", function(event){board.mouseOverCanvas(event);}, false);
+		document.getElementById("playpause").addEventListener("click", function(event){board.clickedPlayPause(event);}, false);
+		document.getElementById("clear").addEventListener("click", function(event){board.clickedClear(event);}, false);
+		document.getElementById("faster").addEventListener("click", function(event){board.clickedFaster(event);}, false);
+		document.getElementById("slower").addEventListener("click", function(event){board.clickedSlower(event);}, false);
+		document.getElementById("zoom-out").addEventListener("click", function(event){board.clickedZoomOut(event);}, false);
+		document.getElementById("zoom-in").addEventListener("click", function(event){board.clickedZoomIn(event);}, false);
 	}
 
   window.onload = main;
